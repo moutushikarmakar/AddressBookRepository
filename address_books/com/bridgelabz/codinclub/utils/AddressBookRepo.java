4bb167da06b1b905af15971e917fda135e4e6bfa
2571405 <63220453+2571405@users.noreply.github.com>
2020-05-07 16:01:56 +0530

    added Address Book Repo

--- address_books/com/bridgelabz/codinclub/utils/AddressBookRepo.java
+++ address_books/com/bridgelabz/codinclub/utils/AddressBookRepo.java
@@ -7,19 +7,43 @@ import com.bridgelabz.codinclub.models.Person;
 import com.bridgelabz.codinclub.services.AddressBookFileOperations;
 import java.util.*;
 
+/**
+*Address Book Repo class
+*@author Moutusi Karmakar
+*/
 public class AddressBookRepo{
-
+	
+	/**
+	*addToAddressBook method adds the new contact to the Address Book CSV file
+	*@param object of type Person
+	*@param ArrayList of type Person
+	*/
 	public void addToAddressBook(final Person person,ArrayList<Person> addPersonList){
 		addPersonList.add(person);
 		AddressBookFileOperations.writeFile(addPersonList);
 	}
+	
+	/**
+	*updateToAddressBook method updates the Address Book CSV file with the chnaged contact
+	*@param Arraylist of type Person
+	*/
 	public void updateToAddressBook(ArrayList<Person> editPersonList){
 		AddressBookFileOperations.writeFile(editPersonList);
 		AddressBookFileOperations.readFile();
 	}
+	
+	/**
+	*deleteFromAddressBook method deletes the contact from the Address Book CSV file
+	*@param Arraylist of type Person
+	*/
 	public void deleteFromAddressBook(ArrayList<Person> deletePersonList){
 		AddressBookFileOperations.writeFile(deletePersonList);
 	}
+	
+	/**
+	*sortAddressBook saves the sorted Address Book in the Address Book CSV file
+	*@param Arraylist of type Person
+	*/
 	public void sortAddressBook(ArrayList<Person> sortPersonList){
 		AddressBookFileOperations.writeFile(sortPersonList);
 		AddressBookFileOperations.readFile();
