ff86b226f30032e61573f9593ed654c60f70805f
2571405 <63220453+2571405@users.noreply.github.com>
2020-05-07 16:37:33 +0530

    added Address Book Services Interface

--- address_books/com/bridgelabz/codinclub/services/AddressBookServices.java
+++ address_books/com/bridgelabz/codinclub/services/AddressBookServices.java
@@ -7,10 +7,38 @@ import com.bridgelabz.codinclub.models.Person;
 import com.bridgelabz.codinclub.services.AddressBookFileOperations;
 import java.util.*;
 
+/**
+*Address Book Sevices interface
+*@author Moutusi Karmakar
+*/
 public interface AddressBookServices{
+	/**
+	*signature of addPerson method
+	*@param addPersonList of type Person
+	*/
 	public void addPerson(ArrayList<Person> addPersonList);
+	
+	/**
+	*signature of editContact method
+	*@param editPersonList of type Person
+	*/
 	public ArrayList<Person> editContact(ArrayList<Person> editPersonList);
+	
+	/**
+	*signature of viewContact method
+	*@param viewContactList of type Person
+	*/
 	public void viewContact(ArrayList<Person> viewPersonList);
+	
+	/**
+	*signature of deleteContact method
+	*@param deleteContactList of type Person
+	*/
 	public ArrayList<Person> deleteContact(ArrayList<Person> deletePersonList);
+	
+	/**
+	*signature of sortContact method
+	*@param sortPersonList of type Person
+	*/
 	public ArrayList<Person> sortContact(ArrayList<Person> sortPersonList);
 }
