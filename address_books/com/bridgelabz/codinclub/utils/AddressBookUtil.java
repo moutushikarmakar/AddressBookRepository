766d1506e179ab9a5a14dee268aeb3d6e51e7f7e
2571405 <63220453+2571405@users.noreply.github.com>
2020-05-07 16:24:58 +0530

    added Address Book Util

--- address_books/com/bridgelabz/codinclub/utils/AddressBookUtil.java
+++ address_books/com/bridgelabz/codinclub/utils/AddressBookUtil.java
@@ -8,24 +8,47 @@ import com.bridgelabz.codinclub.services.AddressBookFileOperations;
 import java.util.Scanner;
 import java.util.*;
 
+/**
+*Address Book Util class
+*@author Moutusi Karmakar 
+*/
 public class AddressBookUtil{
 	private final static Scanner scanner = new Scanner(System.in);
+	
+	/**
+	*method getUserInput takes integer input from the user and returns it
+	*/
 	public static int getUserInput(){
 		return scanner.nextInt();
 	}
-
+	
+	/**
+	*anonymous class
+	*comparator object sortByFname of type Person
+	*compare method returns sorted firstname
+	*/
 	public static Comparator<Person> sortByFname = new Comparator<Person>(){
 		public int compare(Person a, Person b){
 			return (a.firstname).compareTo(b.firstname);
 		}
 	};
 
+	/**
+	*anonymous class
+	*comparator object sortByLname of type Person
+	*compare method returns sorted lastname
+	*/
 	public static Comparator<Person> sortByLname = new Comparator<Person>(){
                 public int compare(Person a, Person b){
                         return (a.lastname).compareTo(b.lastname);
                 }
         };
 
+	/**
+	*anonymous class
+	*comparator object sortByZip of type Person
+	*compare method returns 0,1 and -1 according to the values of Zip 
+	*/
 	public static Comparator<Person> sortByZip = new Comparator<Person>(){
                 public int compare(Person a, Person b){
                         return a.zip-b.zip;
