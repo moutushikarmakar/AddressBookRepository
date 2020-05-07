8cf13af6054a9922b9c3db77e35ae666023d1235
2571405 <63220453+2571405@users.noreply.github.com>
2020-05-07 20:50:14 +0530

    main class

--- address_books/com/bridgelabz/codinclub/main/AddressBookMain.java
+++ address_books/com/bridgelabz/codinclub/main/AddressBookMain.java
@@ -14,6 +14,7 @@ import java.util.*;
 */
 public class AddressBookMain{
 	public static void main(String args[]){
+		System.out.println("------------------WELCOME TO ADDRESS BOOK------------------")
 		/**
 		*Class AddressBookServicesImpl implements the AddressBookServices Interface
 		*creating an arraylist 'personList' that stores each record of the address book
